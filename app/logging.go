61d8ec306f2fa1bcd24764df45c3e5ce0bb04419
Kris Macoskey <kmacoskey@pivotal.io>
2018-03-25 22:40:30 -0600

    Custom logger for more human readable logs during dev

--- app/logging.go
+++ app/logging.go
@@ -1,9 +1,98 @@
 package app
 
 import (
-	log "github.com/sirupsen/logrus"
+	"bytes"
+	"fmt"
 	"os"
+	"sort"
+	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 )
+
+type CustomFormatter struct {
+}
+
+const (
+	nocolor = 0
+	red     = 31
+	green   = 32
+	yellow  = 33
+	blue    = 36
+	gray    = 37
+)
+
+func (f *CustomFormatter) appendKeyValue(b *bytes.Buffer, key string, value interface{}) {
+	if b.Len() > 0 {
+		b.WriteByte(' ')
+	}
+	b.WriteString(key)
+	b.WriteByte('=')
+	f.appendValue(b, value)
+}
+
+func (f *CustomFormatter) appendValue(b *bytes.Buffer, value interface{}) {
+	stringVal, ok := value.(string)
+	if !ok {
+		stringVal = fmt.Sprint(value)
+	}
+
+	if !f.needsQuoting(stringVal) {
+		b.WriteString(stringVal)
+	} else {
+		b.WriteString(fmt.Sprintf("%q", stringVal))
+	}
+}
+
+func (f *CustomFormatter) needsQuoting(text string) bool {
+	return true
+}
+
+func (f *CustomFormatter) printColored(b *bytes.Buffer, entry *log.Entry, keys []string, timestampFormat string) {
+	var levelColor int
+	switch entry.Level {
+	case log.DebugLevel:
+		levelColor = gray
+	case log.WarnLevel:
+		levelColor = yellow
+	case log.ErrorLevel, log.FatalLevel, log.PanicLevel:
+		levelColor = red
+	default:
+		levelColor = blue
+	}
+
+	levelText := strings.ToUpper(entry.Level.String())[0:4]
+
+	sort.Strings(keys)
+
+	// fmt.Fprintf(b, "\x1b[%dm[%s]\x1b[0m[%s] %-100s ", levelColor, levelText, entry.Time.Format(timestampFormat), entry.Message)
+	fmt.Fprintf(b, "\x1b[%dm[%s]\x1b[0m[%s]", levelColor, levelText, entry.Time.Format(timestampFormat))
+	for _, k := range keys {
+		v := entry.Data[k]
+		fmt.Fprintf(b, " \x1b[%dm%s\x1b[0m=", levelColor, k)
+		f.appendValue(b, v)
+	}
+	fmt.Fprintf(b, "\n\n\x1b[%dm%-10s%s\x1b[0m\n", levelColor, " ", entry.Message)
+}
+
+func (f *CustomFormatter) Format(entry *log.Entry) ([]byte, error) {
+	var b *bytes.Buffer
+	keys := make([]string, 0, len(entry.Data))
+	for k := range entry.Data {
+		keys = append(keys, k)
+	}
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+	timestampFormat := time.RFC3339
+
+	f.printColored(b, entry, keys, timestampFormat)
+	b.WriteByte('\n')
+	return b.Bytes(), nil
+}
 
 func InitLogger(config LoggingConfig) error {
 
@@ -12,6 +101,8 @@ func InitLogger(config LoggingConfig) error {
 		log.SetFormatter(&log.TextFormatter{})
 	case "json":
 		log.SetFormatter(&log.JSONFormatter{})
+	case "custom":
+		log.SetFormatter(new(CustomFormatter))
 	default:
 		log.SetFormatter(&log.TextFormatter{})
 	}
